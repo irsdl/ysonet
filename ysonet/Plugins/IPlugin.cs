e8634571b352b347f26d96159101afd2a994f466
Soroush Dalili <1798313+irsdl@users.noreply.github.com>
2025-07-25 23:22:56 +0100

    renaming the solution files
    ysoserial.net -> ysonet rename
    hopefully all will be fine!!

---
+++ ysonet/Plugins/IPlugin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NDesk.Options;
+
+namespace ysonet.Plugins
+{
+    public interface IPlugin
+    {
+        string Name();
+        string Description();
+        string Credit();
+        OptionSet Options();
+        object Run(String[] args);
+    }
+}
